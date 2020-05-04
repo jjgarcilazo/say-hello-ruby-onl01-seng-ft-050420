fd38fcc62c0697b84bdfa75786021e0e5d6fc49e
Jesse Garcilazo <jjgarcilazo@gmail.com>
2020-05-04 23:08:38 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -3,6 +3,6 @@ say_hello("Gabriela")
 
 
 Hello Gabriela!
-def say_hello(name = "Hello Ruby Programmer!)
-  puts " #{name}"
-end)
+def say_hello(name = "Hello Ruby Programmer!")
+  puts "hello, #{name}
+end
